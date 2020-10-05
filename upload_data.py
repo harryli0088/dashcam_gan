35ca75904d1524c2382841c854c06fa62302e840
harryli0088 <harryli0088@gmail.com>
2020-10-05 12:42:04 -0400

    can run hello world

---
+++ upload_data.py
@@ -0,0 +1,4 @@
+from azureml.core import Workspace
+ws = Workspace.from_config()
+datastore = ws.get_default_datastore()
+datastore.upload(src_dir='./data', target_path='image_data_train', overwrite=True)
