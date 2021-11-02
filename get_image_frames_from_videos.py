e3bf6d9c224a095a918b253cd25c103ba5d9ca78
harryli0088 <harryli0088@gmail.com>
2021-11-01 20:10:41 -0400

    updated video parsing script

--- get_image_frames_from_videos.py
+++ get_image_frames_from_videos.py
@@ -22,17 +22,19 @@ for file_path in file_paths:
     vc = cv2.VideoCapture(file_path[0]) # try to open the file as a video
     c=1 # used to name the output image files
 
+    rval = False
     if vc.isOpened():
         rval , frame = vc.read()
-    else:
-        rval = False
 
     while rval: # if there is still data to read
         try:
             rval, frame = vc.read() # read the next frame
-            cv2.imwrite('data/image_data_train/1/' + file_path[1] + "-" + str(c) + '.jpg',frame) # write the image file
-            c = c + 1 # increment the counter
-            cv2.waitKey(1)
+            if frame is not None: # if there is frame data
+              cv2.imwrite('data/image_data_train/1/' + file_path[1] + "-" + str(c) + '.jpg',frame) # write the image file
+              c = c + 1 # increment the counter
+              cv2.waitKey(1)
+            else: # else we are probably at the end of the video
+             break # break out of the loop
         except NameError:
             print(NameError)
 
